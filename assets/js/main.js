4ae27e9958c21b9e9bbe548360797de0cd5cfc62
SuperCoder <propscode254@gmail.com>
2023-09-25 17:29:31 +0200

    test values

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,38 +23,40 @@ const displayChoice = () => {
     : (income2Wrapper.style.display = "block");
 };
 
-// -------------------Help Variables for all years--------------
+// -----------------MAIN FUNCTION
 
-let tax, income, y, z;
-let incomeA, incomeB, incomeC, incomeD;
-let A, B, C, D, E;
+const calculateTax = () => {
+  // asign to year the year input value
+  // asign to income variable
+  let year = Number(yearInput.value);
+  let income = Number(incomeInput1.value);
 
-// ------ Main Function for all years--------------------
+  console.log({ year });
+  console.log(typeof year);
+  console.log({ income });
+  console.log(typeof income);
 
-// case 1
-if (income <= incomeA) {
-  tax = 0;
-  // case 2
-} else if (incomeA + 1 <= income <= incomeB) {
-  y = (income - incomeA) / 10000;
-  tax = (A * y + 1400) * y;
-  // case 3
-} else if (incomeB + 1 <= income <= incomeC) {
-  z = (income - incomeB) / 10000;
-  tax = (B * z + 2397) * z + C;
-  // case 4
-} else if (incomeC + 1 <= income <= incomeD) {
-  tax = 0.42 * income - D;
-  // case 5
-} else if (income >= incomeD + 1) {
-  tax = 0.45 * income - E;
-}
+  // -------------------Help Variables for all years--------------
+  // Give value 0 to be sure values are Numbers
+  let tax = 0;
+  let y = 0;
+  let z = 0;
+  let incomeA = 0;
+  let incomeB = 0;
+  let incomeC = 0;
+  let incomeD = 0;
+  let A = 0;
+  let B = 0;
+  let C = 0;
+  let D = 0;
+  let E = 0;
 
-// --- Values for all the years
+  // console.log(year);
+  // console.log(typeof year);
 
-switch (true) {
-  // -----------------------2023
-  case year === 2023:
+  // ----------------------------------Year Choice--- Values for all the years
+  if (year === 2023) {
+    // -----------------------2023
     incomeA = 10908;
     incomeB = 15999;
     incomeC = 62809;
@@ -65,9 +67,10 @@ switch (true) {
     C = 966.53;
     D = 9972.98;
     E = 18307.73;
-    break;
-  // -----------------------2020
-  case year === 2022:
+    console.log("year23");
+  } else if (year === 2022) {
+    // -----------------------2022
+
     incomeA = 10347;
     incomeB = 14926;
     incomeC = 58596;
@@ -78,9 +81,9 @@ switch (true) {
     C = 869.32;
     D = 9336.45;
     E = 17671.2;
-    break;
-  // -----------------------2021
-  case year === 2021:
+    console.log("year22");
+  } else if (year === 2021) {
+    //-----------------------2021
     incomeA = 9744;
     incomeB = 14753;
     incomeC = 57918;
@@ -91,9 +94,9 @@ switch (true) {
     C = 950.96;
     D = 9136.63;
     E = 17374.99;
-    break;
-  // -----------------------2020
-  case year === 2020:
+    console.log("year21");
+  } else if (year === 2020) {
+    // -----------------------2020
     incomeA = 9408;
     incomeB = 14532;
     incomeC = 57051;
@@ -104,7 +107,56 @@ switch (true) {
     C = 972.79;
     D = 8963.74;
     E = 17078.74;
-    break;
-  default:
-    console.log("No year has been selected ");
-}
+    console.log("year20");
+  } else {
+    console.log("No year has been selected");
+  }
+
+  // ------ Main Function for all years--------------------
+
+  // case 1
+  if (income <= incomeA) {
+    tax = 0;
+    // case 2
+  } else if (incomeA + 1 <= income <= incomeB) {
+    y = (income - incomeA) / 10000;
+    tax = (A * y + 1400) * y;
+    // case 3
+  } else if (incomeB + 1 <= income <= incomeC) {
+    z = (income - incomeB) / 10000;
+    tax = (B * z + 2397) * z + C;
+    // case 4
+  } else if (incomeC + 1 <= income <= incomeD) {
+    tax = 0.42 * income - D;
+    // case 5
+  } else if (income >= incomeD + 1) {
+    tax = 0.45 * income - E;
+  }
+
+  //   ---------Calculate
+
+  console.log({ tax });
+  let roundTax = tax.toFixed(2); //-round only to output
+  outputContainer.innerHTML = `<h3>Für das Jahr ${year} und das Einkommen von ${income} sollen sie </h3><p><span>€</span> ${roundTax} bezahlen<p>`;
+};
+
+//  #test values
+
+// -case1 : 5000/ year 2023
+// tax=0
+
+// -case2: 15000/ year 2023
+
+// y = (15000 - 10908)/10000 = 0.4092
+// tax = (979.18 * y + 1400) * y = 736.838442595
+
+// -case3: 25000/ year 2023
+
+// z= (25000 -15999)/10000 =0.9001
+// tax = (192.59*z + 2397)*z + 966.53 =3280.10226813
+
+// -case4: 75000/ year 2023
+// tax = 0.42 * 75000 -9972.98 = 13192.27
+
+// -case5: 300000/ year 2023
+// tax = 0.45 * 300000 -18307.73 = 116692.27
